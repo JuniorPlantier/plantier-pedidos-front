df7b453dc78ea6f746828fd55b5c3061db29a91f
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-06-04 21:19:48 -0300

    Tratamento padrão para outro erros com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -6,7 +6,7 @@ import { StorageService } from "../services/storage.service";
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>  {
@@ -24,18 +24,52 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status) {
+                    case 401:
+                    this.handle401();
+                    break;
+                    
                     case 403:
                     this.handle403();
                     break;
+
+                    default:
+                    this.handleDefaultEror(errorObj);
                 }
 
                 return Observable.throw(errorObj);
             }) as any;
     }
 
-    // Trata o 403 ai pra mim.
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();        
     }
 }
 
